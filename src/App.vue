42e11d110bca28b0eabd8c761cdcd79216b63445
Aditya Bagus Prakoso <aditya.bagus@elnusa.co.id>
2019-11-13 10:51:22 +0700

    upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+  import Sidebar from './components/Sidebar'
+
+  export default {
+    components: {
+      Sidebar
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import url("https://use.fontawesome.com/releases/v5.11.0/css/all.css");
+  @import url("https://fonts.googleapis.com/css?family=Play");
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 1rem;
+    font-family: "Play", Tahoma;
+    background-color: #ccc;
+    color: #333;
+  }
+
+  #app {
+    position: relative;
+    min-height: 100vh;
+    width: 100%;
+  }
+</style>
---
+++ src/components/SidebarHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="sidebar-header">
+        <div class="logo"></div>
+        <div class="name">Shinchi</div>
+        <div class="group">Minami</div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#sidebar-header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 250px;
+    background: radial-gradient(#f03c39, #920025);
+    color: #fefefe;
+
+    .logo {
+          background-image: url('../assets/logo.png');
+          background-position: center;
+          background-size: cover;
+          width: 120px;
+          height: 120px;
+    }
+
+    .name {
+        font-size: 1.4rem;
+    }
+}
+</style>
